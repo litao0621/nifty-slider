2392915fd23d4a57ae8d32138f9eced70e356f28
李涛 <onresume@live.com>
2023-02-16 14:48:09 +0800

    添加 M3 Style demo

--- simple/src/main/java/com/litao/niftyslider/SimpleActivity.kt
+++ simple/src/main/java/com/litao/niftyslider/SimpleActivity.kt
@@ -3,43 +3,48 @@ package com.litao.niftyslider
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import com.litao.niftyslider.databinding.ActivitySimpleBinding
+import com.litao.niftyslider.fragment.M3StyleDemoFragment
 import com.litao.niftyslider.fragment.ScrollContainerDemoFragment
 
 /**
  * @author : litao
  * @date   : 2023/2/15 16:30
  */
-class SimpleActivity: AppCompatActivity() {
+class SimpleActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySimpleBinding
 
-    companion object{
+    companion object {
         const val EXTRA_ID = "id"
-
     }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         binding = ActivitySimpleBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initView()
     }
 
-    private fun initView(){
-        val id = intent.getIntExtra(EXTRA_ID,0)
-        var fragment:Fragment? = null
+    private fun initView() {
+        val id = intent.getIntExtra(EXTRA_ID, 0)
 
-        when(id){
+        val fragment = when (id) {
             Data.ID_1 -> {
-
+                M3StyleDemoFragment.newInstance()
             }
             Data.ID_2 -> {
-                fragment = ScrollContainerDemoFragment.newInstance()
+                ScrollContainerDemoFragment.newInstance()
+            }
+            else -> {
+                null
             }
         }
+
         if (fragment != null) {
             supportFragmentManager.beginTransaction()
                 .setReorderingAllowed(true)
---
+++ simple/src/main/java/com/litao/niftyslider/fragment/M3StyleDemoFragment.kt
@@ -0,0 +1,48 @@
+package com.litao.niftyslider.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ColorStateListInflaterCompat
+import androidx.fragment.app.Fragment
+import com.litao.niftyslider.R
+import com.litao.niftyslider.databinding.FragmentM3StyleDemoBinding
+
+/**
+ * @author : litao
+ * @date   : 2023/2/16 11:12
+ */
+class M3StyleDemoFragment : Fragment() {
+
+    private lateinit var binding: FragmentM3StyleDemoBinding
+
+
+    companion object {
+        fun newInstance(): M3StyleDemoFragment {
+            return M3StyleDemoFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentM3StyleDemoBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        with(binding){
+            niftySlider.apply {
+                setTrackTintList(ContextCompat.getColorStateList(context, R.color.m3_demo_track_color)!!)
+                setTrackInactiveTintList(ContextCompat.getColorStateList(context, R.color.m3_demo_track_inactive_color)!!)
+                setThumbTintList(ContextCompat.getColorStateList(context, R.color.m3_demo_thumb_color)!!)
+                setThumbShadowColor(ContextCompat.getColor(context,R.color.white))
+            }
+        }
+
+
+    }
+}
