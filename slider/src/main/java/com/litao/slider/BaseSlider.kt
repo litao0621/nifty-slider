38ef30f8401bb6b4c3482e44e74bc34f2d03ec62
李涛 <onresume@live.com>
2023-02-20 20:08:33 +0800

    添加代码注释

--- slider/src/main/java/com/litao/slider/BaseSlider.kt
+++ slider/src/main/java/com/litao/slider/BaseSlider.kt
@@ -285,13 +285,18 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         drawTicks(canvas, trackWidth, yCenter)
 
         if ((isDragging || isFocused) && isEnabled) {
+            //仅在v23以下版本启用此逻辑
             drawCompatHaloIfNeed(canvas, trackWidth, yCenter)
         }
 
         drawThumb(canvas, trackWidth, yCenter)
     }
 
-    fun drawTrack(canvas: Canvas, width: Int, yCenter: Float) {
+    /**
+     * draw active track
+     * 需要考虑如果使用半透明颜色时会与下层[trackColorInactive]颜色进行叠加，需注意叠加后的效果是否满足要求
+     */
+    private fun drawTrack(canvas: Canvas, width: Int, yCenter: Float) {
         trackRectF.set(
             0f + paddingLeft + trackInnerHPadding,
             yCenter - trackHeight / 2f,
@@ -306,7 +311,10 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         )
     }
 
-    fun drawInactiveTrack(canvas: Canvas, width: Int, yCenter: Float) {
+    /**
+     * draw inactive track
+     */
+    private fun drawInactiveTrack(canvas: Canvas, width: Int, yCenter: Float) {
         trackRectF.set(
             0f + paddingLeft + trackInnerHPadding,
             yCenter - trackHeight / 2f,
@@ -321,7 +329,11 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         )
     }
 
-    fun drawThumb(canvas: Canvas, width: Int, yCenter: Float) {
+    /**
+     * Draw thumb
+     * 在[setThumbWithinTrackBounds]模式下，thumb会向内缩进[thumbRadius]距离
+     */
+    private fun drawThumb(canvas: Canvas, width: Int, yCenter: Float) {
         canvas.withTranslation(
             (paddingLeft + trackInnerHPadding + thumbOffset + (percentValue(value) * (width - thumbOffset * 2)).toInt()) - defaultThumbDrawable.bounds.width() / 2f,
             yCenter - (defaultThumbDrawable.bounds.height() / 2f)
@@ -331,8 +343,8 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
     }
 
     /**
-     * draw compat halo
-     * 绘制thumb光环效果
+     * Draw compat halo
+     * 绘制滑块的光环效果
      */
     private fun drawCompatHaloIfNeed(canvas: Canvas, width: Int, yCenter: Float) {
         if (shouldDrawCompatHalo()) {
@@ -349,6 +361,9 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
     }
 
 
+    /**
+     * draw tick
+     */
     private fun drawTicks(canvas: Canvas, width: Int, yCenter: Float) {
         if (enableStepMode()) {
             val drawWidth = width - thumbOffset * 2 - tickRadius * 2
@@ -376,17 +391,17 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         }
     }
 
-    fun enableStepMode(): Boolean {
-        return stepSize > 0 && tickVisible
-    }
-
+    /**
+     * Returns a number between 0 and 1 with [BaseSlider.value]
+     * 通过value返回当前滑动百分比，0为最左、1为最右
+     */
     private fun percentValue(value: Float): Float {
         return (value - valueFrom) / (valueTo - valueFrom)
     }
 
 
     /**
-     * this method is called before the onDraw.make sure parameter is valid
+     * This method is called before the onDraw.make sure parameter is valid
      * 对可能存在的脏数据进行校验或修正
      */
     private fun validateDirtyData() {
@@ -398,18 +413,27 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         }
     }
 
+    /**
+     * 校验[valueFrom]合法性
+     */
     private fun validateValueFrom() {
         if (valueFrom > valueTo) {
             throw IllegalStateException("valueFrom($valueFrom) must be smaller than valueTo($valueTo)")
         }
     }
 
+    /**
+     * 校验[valueTo]合法性
+     */
     private fun validateValueTo() {
         if (valueTo <= valueFrom) {
             throw IllegalStateException("valueTo($valueTo) must be greater than valueFrom($valueFrom)")
         }
     }
 
+    /**
+     * 校验[BaseSlider.value]合法性，对不合法数据进行修正
+     */
     private fun validateValue() {
         //value 超出起始结束范围则进行修正
         if (value < valueFrom) {
@@ -429,6 +453,10 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
     }
 
 
+    /**
+     * Returns true if view height changed
+     * 检查高度是否发生变化，变化后更新当前记录高度
+     */
     fun viewHeightChanged(): Boolean {
         val topBottomPadding = paddingTop + paddingBottom
         val minHeightWithTrack = topBottomPadding + trackHeight
@@ -444,14 +472,35 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         }
     }
 
+    /**
+     * update track real draw width
+     * 更新滑轨真实绘制宽度，真实宽度不仅受左右padding影响，还会受内部[trackInnerHPadding]影响
+     */
     fun updateTrackWidth(viewWidth: Int) {
         trackWidth = max(viewWidth - paddingLeft - paddingRight - trackInnerHPadding * 2, 0)
+    }
+
+    /**
+     * Sets the slider's [BaseSlider.value]
+     * 如果存在step size时 value 可能会根据step size进行修正
+     *
+     * @param value 必须小于等于 [valueTo] 大于等于 [valueFrom]
+     */
+    fun setValue(value: Float) {
+        if (this.value != value) {
+            this.value = value
+            hasDirtyData = true
+            onValueChanged(value, false)
+            postInvalidate()
+        }
     }
 
     /**
      * Sets the horizontal inner padding of the track.
      * 主要处理thumb超出部分的视图，使thumb展示正常
      * 也可以使用 [BaseSlider.setThumbWithinTrackBounds] 来将thumb直接控制在track内部
+     *
+     * @see R.attr.trackInnerHPadding
      *
      * @param padding track左右的padding值，
      */
@@ -480,6 +529,8 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
      * Sets the vertical inner padding of the track.
      * 主要处理thumb阴影超出部分的视图，使thumb展示正常
      *
+     * @see R.attr.trackInnerVPadding
+     *
      * @param padding track左右的padding值，
      */
     fun setTrackInnerVPadding(padding: Int) {
@@ -498,6 +549,11 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
     }
 
 
+    /**
+     * Sets the color for the track
+     *
+     * @see R.attr.trackColor
+     */
     fun setTrackTintList(color: ColorStateList) {
         if (this::trackColor.isInitialized && color == trackColor) {
             return
@@ -507,6 +563,13 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         invalidate()
     }
 
+    /**
+     * Sets the color for the secondary track
+     *
+     * @see R.attr.trackSecondaryColor
+     *
+     * eg.视频滑动进度条可能存在缓存进度,通过此方法来改变二级滑轨颜色
+     */
     fun setTrackSecondaryTintList(color: ColorStateList) {
         if (this::trackSecondaryColor.isInitialized && color == trackSecondaryColor) {
             return
@@ -516,6 +579,11 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         invalidate()
     }
 
+    /**
+     * Sets the inactive color for the track
+     *
+     * @see R.attr.trackColorInactive
+     */
     fun setTrackInactiveTintList(color: ColorStateList) {
         if (this::trackColorInactive.isInitialized && color == trackColorInactive) {
             return
@@ -525,6 +593,11 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         invalidate()
     }
 
+    /**
+     * Sets the color for the tick
+     *
+     * @see R.attr.ticksColor
+     */
     fun setTicksTintList(color: ColorStateList) {
         if (this::ticksColor.isInitialized && color == ticksColor) {
             return
@@ -534,6 +607,11 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         invalidate()
     }
 
+    /**
+     * Sets the inactive color for the tick
+     *
+     * @see R.attr.ticksColorInactive
+     */
     fun setTicksInactiveTintList(color: ColorStateList) {
         if (this::ticksColorInactive.isInitialized && color == ticksColorInactive) {
             return
@@ -543,6 +621,12 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         invalidate()
     }
 
+    /**
+     * Sets the radius of the tick in pixels.
+     * 设置刻度半径大小
+     *
+     * @see R.attr.tickRadius
+     */
     fun setTickRadius(@FloatRange(from = 0.0) @Dimension tickRadius: Float) {
         if (this.tickRadius != tickRadius) {
             this.tickRadius = tickRadius
@@ -550,6 +634,14 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         }
     }
 
+    /**
+     * Sets the radius of the thumb in pixels.
+     * 设置滑块半径大小
+     *
+     * @see R.attr.thumbRadius
+     *
+     * @param radius 滑块半径
+     */
     fun setThumbRadius(@IntRange(from = 0) @Dimension radius: Int) {
         if (radius == thumbRadius) {
             return
@@ -570,6 +662,8 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
      * Sets whether the thumb within track bounds
      * 正常模式下滑块thumb是以track的起始位置为中心,thumb较大时左半部分会超出视图边界
      * 某些样式下，thumb需要控制在track的范围以内，可通过此方法来启用此项功能
+     *
+     * @see R.attr.thumbWithinTrackBounds
      *
      * @param isInBounds thumb 是否需要绘制在 track 范围以内
      */
@@ -592,6 +686,11 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         updateViewLayout()
     }
 
+    /**
+     * Sets the color of the thumb.
+     *
+     * @see R.attr.thumbColor
+     */
     fun setThumbTintList(thumbColor: ColorStateList) {
         if (thumbColor == defaultThumbDrawable.fillColor) {
             return
@@ -600,12 +699,20 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         invalidate()
     }
 
+
+    /**
+     * Sets the color of the halo.
+     * 设置滑块点击后光环颜色
+     *
+     * @see R.attr.haloColor
+     */
     fun setHaloTintList(haloColor: ColorStateList) {
         if (this::haloColor.isInitialized && this.haloColor == haloColor) {
             return
         }
 
         this.haloColor = haloColor
+        //v23以下通过绘制实现，仅修改画笔颜色即可
         if (!shouldDrawCompatHalo() && background is RippleDrawable) {
             (background as RippleDrawable).setColor(haloColor)
             return
@@ -620,12 +727,19 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
 
     }
 
+    /**
+     * Sets the radius of the halo in pixels.
+     * 设置滑块点击后光环的半径
+     *
+     * @see R.attr.haloRadius
+     */
     fun setHaloRadius(@IntRange(from = 0) @Dimension radius: Int) {
         if (haloRadius == radius) {
             return
         }
 
         haloRadius = radius
+        //v23以下通过绘制实现，v23以上通过hook ripple effect background来修改半径
         if (!shouldDrawCompatHalo() && enableDrawHalo && background is RippleDrawable) {
             hookRippleRadius(background as RippleDrawable, haloRadius)
             return
@@ -633,26 +747,55 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
         postInvalidate()
     }
 
+    /**
+     * Sets the elevation of the thumb.
+     *
+     * @see R.attr.thumbElevation
+     */
     fun setThumbElevation(elevation: Float) {
         defaultThumbDrawable.elevation = elevation
 
         thumbElevation = elevation
     }
 
+    /**
+     * Sets the stroke color for the thumbs
+     *
+     * @see R.attr.thumbStrokeColor
+     */
     fun setThumbStrokeColor(thumbStrokeColor: ColorStateList?) {
         defaultThumbDrawable.strokeColor = thumbStrokeColor
         postInvalidate()
     }
 
+    /**
+     * Sets the stroke width for the thumb
+     *
+     * @see R.attr.thumbStrokeWidth
+     */
     fun setThumbStrokeWidth(thumbStrokeWidth: Float) {
         defaultThumbDrawable.strokeWidth = thumbStrokeWidth
         postInvalidate()
     }
 
+    /**
+     * Sets the shadow width for the thumb
+     *
+     * @see R.attr.thumbShadowColor
+     */
     fun setThumbShadowColor(@ColorInt shadowColor: Int) {
         defaultThumbDrawable.setShadowColor(shadowColor)
     }
 
+
+    /**
+     * Sets whether the halo should be draw
+     * 启用光环效果
+     *
+     * @see R.attr.enableDrawHalo
+     *
+     * @param enable True if this enable draw halo
+     */
     fun setEnableDrawHalo(enable: Boolean) {
         enableDrawHalo = enable
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && haloDrawable == null && enable) {
@@ -660,6 +803,105 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
             haloDrawable = background as RippleDrawable
         }
     }
+
+    /**
+     * Returns true if step mode enable
+     * 是否启用了刻度功能
+     */
+    fun enableStepMode(): Boolean {
+        return stepSize > 0 && tickVisible
+    }
+
+    /**
+     * Update halo Hotspot coordinate
+     *
+     * 仅在v23及以上生效，更新ripple effect坐标
+     */
+
+
+    fun shouldDrawCompatHalo(): Boolean {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || background !is RippleDrawable
+    }
+
+
+    @ColorInt
+    fun getColorForState(colorStateList: ColorStateList): Int {
+        return colorStateList.getColorForState(drawableState, colorStateList.defaultColor)
+    }
+
+    /**
+     * 如果启用了刻度功能，对当前滑动坐标进行修改，定位到临近刻度
+     */
+    private fun snapStepPos(pos: Float): Float {
+        if (enableStepMode()) {
+            val stepCount = ((valueTo - valueFrom) / stepSize).toInt()
+            return (pos * stepCount).roundToInt() / stepCount.toFloat()
+        }
+        return pos
+    }
+
+    /**
+     * 通过当前滑动位置百分比转化为准确的value值
+     * 起始、结束值可能存在多种类型
+     *
+     * eg.
+     * valueFrom = -40  valueTo = -20
+     * valueFrom = -1  valueTo = 1
+     * valueFrom = 1  valueTo = 100
+     * valueFrom = 50  valueTo = 80
+     *
+     */
+    private fun getValueByTouchPos(pos: Float): Float {
+        val position = snapStepPos(pos)
+        return position * (valueTo - valueFrom) + valueFrom
+    }
+
+    /**
+     * 通过当前坐标获取滑动位置百分比
+     */
+    private fun getTouchPosByX(touchX: Float): Float {
+        return MathUtils.clamp((touchX - paddingLeft - trackInnerHPadding) / trackWidth, 0f, 1f)
+    }
+
+    /**
+     * 是否在纵向滚动的容器中
+     * 此情况下需要对touch event做特殊处理
+     */
+    private fun isInVerticalScrollingContainer(): Boolean {
+        var p = parent
+        while (p is ViewGroup) {
+            val parent = p
+            val canScrollVertically = parent.canScrollVertically(1) || parent.canScrollVertically(-1)
+            if (canScrollVertically && parent.shouldDelayChildPressedState()) {
+                return true
+            }
+            p = p.getParent()
+        }
+        return false
+    }
+
+
+    /**
+     * Start drag slider
+     */
+    private fun startTacking(event: MotionEvent) {
+        isTackingStart = true
+        onStartTacking()
+    }
+
+    /**
+     * stop drag slider
+     */
+    private fun stopTacking(event: MotionEvent) {
+        if (isTackingStart) {
+            onStopTacking()
+        }
+        isTackingStart = false
+        invalidate()
+    }
+
+
+
 
     private fun updateHaloHotspot() {
         if (enableDrawHalo) {
@@ -678,70 +920,6 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
                     )
                 }
             }
-        }
-    }
-
-    fun shouldDrawCompatHalo(): Boolean {
-        return Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || background !is RippleDrawable
-    }
-
-
-    @ColorInt
-    fun getColorForState(colorStateList: ColorStateList): Int {
-        return colorStateList.getColorForState(drawableState, colorStateList.defaultColor)
-    }
-
-    private fun snapStepPos(pos: Float): Float {
-        if (enableStepMode()) {
-            val stepCount = ((valueTo - valueFrom) / stepSize).toInt()
-            return (pos * stepCount).roundToInt() / stepCount.toFloat()
-        }
-        return pos
-    }
-
-    private fun getValueByTouchPos(pos: Float): Float {
-        val position = snapStepPos(pos)
-        return position * (valueTo - valueFrom) + valueFrom
-    }
-
-    private fun getTouchPosByX(touchX: Float): Float {
-        return MathUtils.clamp((touchX - paddingLeft - trackInnerHPadding) / trackWidth, 0f, 1f)
-    }
-
-    private fun isInVerticalScrollingContainer(): Boolean {
-        var p = parent
-        while (p is ViewGroup) {
-            val parent = p
-            val canScrollVertically = parent.canScrollVertically(1) || parent.canScrollVertically(-1)
-            if (canScrollVertically && parent.shouldDelayChildPressedState()) {
-                return true
-            }
-            p = p.getParent()
-        }
-        return false
-    }
-
-
-    private fun startTacking(event: MotionEvent) {
-        isTackingStart = true
-        onStartTacking()
-    }
-
-    private fun stopTacking(event: MotionEvent) {
-        if (isTackingStart) {
-            onStopTacking()
-        }
-        isTackingStart = false
-        invalidate()
-    }
-
-
-    fun setValue(value: Float) {
-        if (this.value != value) {
-            this.value = value
-            hasDirtyData = true
-            onValueChanged(value, false)
-            postInvalidate()
         }
     }
 
@@ -817,6 +995,12 @@ abstract class BaseSlider constructor(context: Context, attrs: AttributeSet? = n
     }
 
 
+    /**
+     * Returns true if current touch event is click event
+     *
+     * @param startEvent 滑动过程的down事件
+     * @param endEvent   滑动过程的up事件
+     */
     private fun isClickTouch(startEvent: MotionEvent, endEvent: MotionEvent): Boolean {
         val differenceX = abs(startEvent.x - endEvent.x)
         val differenceY = abs(startEvent.y - endEvent.y)
